9a5c4c96b51ba800121d5b76f41ba78d3253ff43
charlesnyankieya <nyankieyao@gmail.com>
2022-11-03 14:44:39 +0300

    Landing Activity with both register and login buttons

--- app/src/main/java/com/example/firebase/LandingActivity.java
+++ app/src/main/java/com/example/firebase/LandingActivity.java
@@ -1,14 +1,40 @@
 package com.example.firebase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class LandingActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        AppCompatButton btnRegister = findViewById(R.id.btn_register);
+        btnRegister.setOnClickListener(v -> {
+
+            startActivity(new Intent(LandingActivity.this, RegisterActivity.class));
+            finish();
+
+        });
+
+        TextView txtLogin = findViewById(R.id.txt_login);
+        txtLogin.setOnClickListener(v -> {
+            startActivity(new Intent(LandingActivity.this, LoginActivity.class));
+            finish();
+
+        });
+
+
+
+
     }
 }
