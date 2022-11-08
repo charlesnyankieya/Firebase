dce1d41615ac2a35c20ae49ec3f180e2424814fa
charlesnyankieya <nyankieyao@gmail.com>
2022-11-08 21:54:15 +0300

    Designing the Login in layout, with email, and password edittexta, together with a login button

---
+++ src/main/java/com/example/firebase/SplashscreenActivity.java
@@ -0,0 +1,24 @@
+package com.example.firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashscreenActivity.this, LandingActivity.class));
+                finish();
+            }
+        },1500);
+    }
+}
