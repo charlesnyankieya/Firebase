59a9ea7066ab27d0a42fec741956b088402c9898
charlesnyankieya <nyankieyao@gmail.com>
2022-11-08 23:53:25 +0300

    Designing the layout for login activity with edittexts for passwords and emails and a button to submit

--- src/main/java/com/example/firebase/LandingActivity.java
+++ src/main/java/com/example/firebase/LandingActivity.java
@@ -29,10 +29,11 @@ public class LandingActivity extends AppCompatActivity {
         TextView txtLogin = findViewById(R.id.txt_login);
         txtLogin.setOnClickListener(v -> {
             startActivity(new Intent(LandingActivity.this, LoginActivity.class));
+            //finish();
 
         });
 
 
-        
+
     }
 }
--- src/main/java/com/example/firebase/LoginActivity.java
+++ src/main/java/com/example/firebase/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.firebase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -11,6 +12,11 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        Toolbar loginToolbar = (Toolbar) findViewById(R.id.login_toolbar);
+      // loginToolbar.setTitle(null);
+        setSupportActionBar(loginToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 
 }
